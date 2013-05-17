e1ba5e6584eab689e1384a8ab51e44ed6a30b857
Sebastian Blatt <no@spam.tld>
2013-05-17 17:54:33 -0400

    Also copy .cc file on new project.

--- src/create_new_tool.sh
+++ src/create_new_tool.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # -*- mode: Sh -*-
-# Time-stamp: "2013-05-17 14:04:06 sb"
+# Time-stamp: "2013-05-17 17:17:38 sb"
 
 #  file       create_new_tool.sh
 #  copyright  (c) Sebastian Blatt 2013
 
 project=$1
 vc_project_file=${project}/${project}.vcxproj
+project_source=${project/${project}.cc
 
-echo "This script creates a new subdirectory and a corresponding"
-echo "Visual Studio .vcxproj file."
+echo "This script creates a new subdirectory and corresponding"
+echo ".cc and Visual Studio .vcxproj files."
 
 mkdir "${project}"
 
 cp lsvisa/lsvisa.vcxproj "${vc_project_file}" && \
-    sed -E -i "s/lsvisa/${project}/g" "${vc_project_file}"
+    sed -E -i "s/lsvisa/${project}/g" "${vc_project_file}" && \
+    cp lsvisa/lsvisa.cc "${project_source}" && \
+    sed -E -i "s/lsvisa/${project}/g" "${project_source}"
